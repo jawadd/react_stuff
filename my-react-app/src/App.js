9419ccf0135e1593ef73bc7268f6ad1608dc713b
Jawad Hussain <jawadduet@gmail.com>
2024-09-10 12:42:16 +0500

    Create a basic static page using components and styling

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from "./components/NavBar"
+import Main from "./components/MainContent"
+
+function App() {
+ 
+  return (
+    <div className="container">
+        <Navbar />
+        <Main />
+    </div>
+)
+  
+}
+
+export default App;
