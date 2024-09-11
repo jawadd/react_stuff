97eca314a9f0e9d63b19989cf64e7a0468b591b6
Jawad Hussain <jawadduet@gmail.com>
2024-09-11 10:59:45 +0500

    Create Navbar and Hero

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from "./components/NavBar"
 import Main from "./components/MainContent"
+import Hero from "./components/Hero"
+
 
 function App() {
  
   return (
     <div className="container">
         <Navbar />
-        <Main />
-    </div>
+        <Hero />
+      
+          </div>
 )
   
 }
